1f4ac452045c6707cd804718eace4e401cd138bc
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-05-02 16:19:10 +0200

    [add] public function setFasciaEta()

--- index.php
+++ index.php
@@ -4,12 +4,22 @@ class Movie {
   public $title;
   public $genre;
   public $year;
+  public $VM18 = false;
 
+  // costruttore
   public function __construct(string $_title, array $_genre, int $_year)
   {
     $this->title = $_title;
     $this->genre = $_genre;
     $this->year = $_year;
+    $this->setFasciaEta();
+  }
+
+  // metodo
+  public function setFasciaEta(){
+    if(in_array('Horror', $this->genre)){
+      $this->VM18 = true;
+    }
   }
 }
 
@@ -18,7 +28,14 @@ $matrix = new Movie('Matrix', ['Action', 'Sci-fi'], 1999);
 // $matrix->genre = 'Action';
 // $matrix->year = '1999';
 
+$barbie = new Movie('Barbie', ['Comedy', 'Fantasy'], 2023);
+
+$sinister = new Movie('Sinister', ['Horror', 'Mistery'], 2012);
+
+
 var_dump($matrix);
+var_dump($barbie);
+var_dump($sinister);
 
 ?>
 
